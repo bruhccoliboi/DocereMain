ba3ee99d3d1ad44157f6c06fbf5b6f27da8fe217
Shivam Shiragavi <shivam.s.shiragavi@gmail.com>
2026-05-31 20:09:28 +0530

    more gap between learner cards

--- src/app/(app)/learners/page.tsx
+++ src/app/(app)/learners/page.tsx
@@ -196,7 +196,7 @@ function LearnersContent() {
             }
           />
         ) : (
-          <div className="space-y-6">
+          <div className="space-y-8">
             {displayed.map((learner) => {
               const billing = billingProfiles.find(
                 (b) => b.learnerId === learner.id,
